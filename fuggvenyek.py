049496ecb067b43b17c26ad00fbea762f61ca7f4
SzudaTiborSzilveszter <szudatiborszilveszter@gmail.com>
2025-10-21 14:40:51 +0200

    Számlázó ver 1

---
+++ fuggvenyek.py
@@ -0,0 +1,13 @@
+def szamlazo(foodlista):
+    print("=" * 50)
+    print(" " * 20 + "Hekk Tuah")
+    print("=" * 50)
+    osszeg=0
+    index=0
+    print()
+    print("-" *50)
+    print(f"{'VÉGÖSSZEG:':<30} {osszeg:>16} Ft")
+    print("=" * 50)
+    print()
+    print(" "* 14 + "Köszönjük a vásárlását!")
+    print("=" * 50)
