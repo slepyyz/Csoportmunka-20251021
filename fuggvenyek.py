357d29efdc37db72253800fd5aeb77f9490d6731
slepyyz <szontaghagoston@gmail.com>
2025-10-21 14:42:14 +0200

    Nemtudom

--- fuggvenyek.py
+++ fuggvenyek.py
@@ -4,5 +4,5 @@ etelek=[
     {"nev": "Paradicsomleves", "ar": 2499, "tipus": "eloetel"}
     {"nev": "Steak", "ar": 1999, "tipus": "foetel"}
     {"nev": "Körömpörkölt", "ar": 3499, "tipus": "foetel"}
-    {"nev": "The fortress Stilt Fisherman Idulgence", "ar": 5600000, "tipus": "foetel"}
+    {"nev": "The fortress Stilt Fisherman Idulgence", "ar": 5600000, "tipus": "eloetel"}
 ]
