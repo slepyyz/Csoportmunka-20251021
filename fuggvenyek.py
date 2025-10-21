1d0bc2f729dd49219bdb843a39c872536ee059a5
slepyyz <szontaghagoston@gmail.com>
2025-10-21 15:07:13 +0200

    rendezes main.py

--- fuggvenyek.py
+++ fuggvenyek.py
@@ -1,12 +1,5 @@
-etelek=[
-    {"nev": "Húsleves", "ar": 1999, "tipus": "eloetel"},
-    {"nev": "Gyümölcsleves", "ar": 3499, "tipus": "eloetel"},
-    {"nev": "Paradicsomleves", "ar": 2499, "tipus": "eloetel"},
-    {"nev": "Steak", "ar": 1999, "tipus": "foetel"},
-    {"nev": "Körömpörkölt", "ar": 3499, "tipus": "foetel"},
-    {"nev": "The fortress Stilt Fisherman Idulgence", "ar": 5600000, "tipus": "foetel"}
-]
-    
+
+
 def osszegzo(foodlista):
     osszeg = 0
     hossz = 0
@@ -15,7 +8,7 @@ def osszegzo(foodlista):
         hossz += 1
     return osszeg
 
-def szamlazo(foodlista,osszeg,hossz):
+def szamlazo(foodlista,osszeg):
     print("=" * 50)
     print(" " * 20 + "Hekk Tuah")
     print("=" * 50)
